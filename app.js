9bd27daebbb8a0c2dfb53f10681edf6668d63bd2
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-07-15 18:10:39 -0300

    cargar

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const express = require('express');
+
+// node-mysql-app/app.js
+
+const express = require("express");
+const { connection } = require("./db");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send ('Hola, como va?');
-})
+app.get("/api/capitulos", (req, res) => {
+  connection.query("SELECT * FROM capitulook", (err, data) => {
+    if (err) return callback(err, null);
+    res.status(200).json({
+      status: "success",
+      length: data.length,
+      data,
+    });
+  });
+});
 
-app.listen(8080, () => console.log (`Server escuchando en ${8080}`));
+app.listen(8080, () => console.log(`Server is listening on port ${8080}`));
