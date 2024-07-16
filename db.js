f134d4682498729f9a1ee9360b066aa055e23e98
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-07-16 18:19:23 -0300

    guardar ok

--- db.js
+++ db.js
@@ -4,17 +4,37 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 // Replace the hard-coded values with the env variables
-connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-});
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DB,
+//   connectTimeout: 12000
+// });
+
+// Crear conexión usando la URL completa
+const connection = mysql.createConnection(process.env.MYSQL_URL);
+
+
+
+
 connection.connect((err) => {
   if (err) {
     console.error("CONNECT FAILED", err.code);
-  } else console.log("CONNECTED");
+    return;
+  }
+console.log("CONNECTED to the database");
 });
+
+// Ejemplo de una consulta
+connection.query('SELECT * FROM capitulook', (err, results, fields) => {
+  if (err) {
+      console.error('Error in query:', err);
+      return;
+  }
+  console.log('Query results:', results);
+});
+
 
 module.exports = { connection };
