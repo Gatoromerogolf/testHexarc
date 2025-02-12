827b5b0206dfe73c641830553f59ad208e35e7d4
Gatoromerogolf <ruben.e.garcia@gmail.com>
2025-02-12 15:38:48 -0300

    formulario

---
+++ public/js/loginreg.js
@@ -0,0 +1,46 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Realiza la solicitud al servidor para validar las credenciales
+    fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message === 'Login exitoso') {
+            // Guardar el nombre de usuario en localStorage
+            localStorage.setItem('username', data.user.username);
+            localStorage.setItem('ingresado', data.user.ingresado); 
+            localStorage.setItem('nombre', data.user.firstName);
+            localStorage.setItem('apellido', data.user.lastName);
+            localStorage.setItem('CUIT', data.user.CUIT);
+            localStorage.setItem('empresa', data.user.empresa);
+            localStorage.setItem('idioma', data.user.idioma);
+            localStorage.setItem('ria', data.user.ria);
+            localStorage.setItem('servicio', data.user.servicio);
+
+            if (data.user.ingresado == 0) {
+                window.location.href = '../registro3.html';
+               } else {
+                if (data.user.idioma == 1){
+                window.location.href = '../registro3.html'}
+                else{
+                    window.location.href = '../registro3-en.html'}
+                }             
+            }
+        else {
+            console.error('Credenciales inválidas');
+        }
+    })
+    .catch(error => {
+        alert("usuario o clave invalidos");
+        console.error('Error en la solicitud:', error);
+    });
+});
